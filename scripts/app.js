1d2bf87291c5de3743d3045b93b5bbd1a7aab91b
Smeshnik <ermachkovstas@mail.ru~>
2022-07-01 21:39:34 +0300

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+const container = document.querySelector('.container');
+const movieTitle = document.querySelector('.userInputTitle');
+const moviePosterUrl = document.querySelector('.userInputPosterUrl');
+const movieYearofPublication = document.querySelector('.userYearofPublication');
+const btn = document.querySelector('.button');
+const movieTitleToDisplay = document.querySelector('.favoriteMovieTitle');
+
+
+let titleInStorage = localStorage.getItem('title');
+let imageUrlInStorage = localStorage.getItem('imageUrl');
+let yearInStorage = localStorage.getItem('year');
+
+if(titleInStorage && imageUrlInStorage && yearInStorage) {
+    movieTitleToDisplay.textContent = titleInStorage;
+    container.style.backgroundImage = linear-gradient(rgba(51, 51, 82, 0.685), rgba(73, 49, 49, 0.582)), 
+    url('${imageUrlInStorage}');
+}
+
+
+
+btn.addEventListener('click', () => {
+    let movieTitleInput = movieTitle.value;
+    let posterUrlInput = moviePosterUrl.value;
+    localStorage.setItem('title', movieTitleInput);
+    localStorage.setItem('imageUrl', posterUrlInput);
+    localStorage.setItem('year',yearInput);
+    movieTitleToDisplay.textContent = movieTitleInput;
+    container.style.backgroundImage = linear-gradient(rgba(51, 51, 82, 0.685), rgba(73, 49, 49, 0.582)), 
+    url('${posterUrlInput}');
+    movieTitle.value = '';
+    moviePosterUrl.value = '';
+});
